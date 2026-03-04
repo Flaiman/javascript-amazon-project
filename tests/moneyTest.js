050eac35eeb59410a70637f1c0b50c8ae205cc68
Flaiman <mrflaiman@gmail.com>
2026-03-04 16:09:09 +0200

    Jasmine test

---
+++ tests/moneyTest.js
@@ -0,0 +1,13 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+if (formatCurrency(2095)==='20.95'){
+    console.log('Passed')
+} else{
+    console.log('not')
+}
+
+if(formatCurrency(0)==='0.00'){
+    console.log('Passed')
+} else{
+    console.log('Failed')
+}
